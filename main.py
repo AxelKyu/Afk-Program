a1b0c3d04c57322d8557ccce2e303232615619ea
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-09-20 10:13:40 +0545

    Added keyboard library

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pyautogui as pag
 import time
 import random
+import keyboard
 
 # Pointing the mouse at a different direction
 while True:
-    x = random.randint(400, 800)
-    y = random.randint(400, 800)
-    pag.moveTo(x, y, 1.0)
-    time.sleep(2)
+    if keyboard.read_key() == "0":
+        break
+    else:
+        x = random.randint(400, 800)
+        y = random.randint(400, 800)
+        pag.moveTo(x, y, 1.0)
+        time.sleep(2)
+
 
