b2710dbbd3bfa5a7d33095e07b5c2cdaa8df20b3
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-09-20 10:36:45 +0545

    Pc doesnt go to sleep when the program is runing

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import ctypes
 import pyautogui as pag
 import time
 import random
 import keyboard
 
-# Pointing the mouse at a different direction
-while True:
-    time.sleep(1)
-    if keyboard.is_pressed("0"):
-        break
-    else:
-        x = random.randint(400, 800)
-        y = random.randint(400, 800)
-        pag.moveTo(x, y, 1.0)
+# Prevent the system from going to sleep
+ctypes.windll.kernel32.SetThreadExecutionState(0x80000002)
 
-
+try:
+    # Pointing the mouse at a different direction
+    while True:
+        time.sleep(1)
+        if keyboard.is_pressed("0"):
+            break
+        else:
+            x = random.randint(400, 800)
+            y = random.randint(400, 800)
+            pag.moveTo(x, y, 1.0)
+finally:
+    # Allow the system to sleep again after the program ends
+    ctypes.windll.kernel32.SetThreadExecutionState(0x80000000)
