51bb919c39c24f154c20be55944620f5cb8c6f2f
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-09-20 10:43:20 +0545

    Removed Mouse Movement

--- main.py
+++ main.py
@@ -13,10 +13,6 @@ try:
         time.sleep(1)
         if keyboard.is_pressed("0"):
             break
-        else:
-            x = random.randint(400, 800)
-            y = random.randint(400, 800)
-            pag.moveTo(x, y, 1.0)
 finally:
     # Allow the system to sleep again after the program ends
     ctypes.windll.kernel32.SetThreadExecutionState(0x80000000)
