000259c074bddd7a43bf24aff11736a28f07b7ad
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-09-20 10:33:52 +0545

    Timing for exiting program improved

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import keyboard
 
 # Pointing the mouse at a different direction
 while True:
-    if keyboard.read_key() == "0":
+    time.sleep(1)
+    if keyboard.is_pressed("0"):
         break
     else:
         x = random.randint(400, 800)
         y = random.randint(400, 800)
         pag.moveTo(x, y, 1.0)
-        time.sleep(2)
 
 
